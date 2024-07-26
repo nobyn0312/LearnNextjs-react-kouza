ccd2bc080a6d9d0cdf4e4fdc7879eaae8c68c513
nobyn0312 <nobyn0312@gmail.com>
2024-07-26 19:00:51 +0900

    first

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { Footer } from "../../components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,9 +16,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
+
+      <header/>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            ochim &nbsp;
             <code className={styles.code}>src/pages/index.js</code>
           </p>
           <div>
@@ -108,6 +111,8 @@ export default function Home() {
             </p>
           </a>
         </div>
+
+        <Footer/>
       </main>
     </>
   );
