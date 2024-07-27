f6c1ad5aadc082b4a3169a668d7cfa50a858b170
nobyn0312 <nobyn0312@gmail.com>
2024-07-27 19:07:31 +0900

    mainのコンポーネント化

--- components/Headline.js
+++ components/Headline.js
@@ -3,15 +3,13 @@ import styles from "@/styles/Home.module.css";
 
 
 export default function Headline(props) {
-  console.log(props)
   return (
     <>
     <h1 className={styles.title}>{props.page} Page</h1>
-
     <p className={styles.description}>
     get started
     </p>
-    <code className={styles.code}>pages/{props.page}.js</code>
+    <code className={styles.code}>pages/{props.children}</code>
     </>
   );
 }
