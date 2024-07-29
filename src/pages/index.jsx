a68dc80054f968a6960f8999c81f3375411bf005
nobyn0312 <nobyn0312@gmail.com>
2024-07-29 19:10:35 +0900

    useCallbackは再生成されない

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,9 +11,12 @@ export default function Home() {
   const [count, setCount] = useState(1);
 
 
-  const handleClick = (e) => {
-    setCount((count) => count + 1);
-  }
+  const handleClick = useCallback(
+    (e) => {
+      if(count <10){
+        setCount((count) => count + 1);
+      }
+    },[])
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue"
@@ -30,8 +33,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <div style={{margin:"0 auto",textAlign:"center"}}>
       <h1>{count}</h1>
       <button onClick={handleClick}>ボタン</button>
+      </div>
 
       <Main page="index" />
       <Footer />
