26a18b59760e6b6caf17a042dec92e2e05e173b1
nobyn0312 <nobyn0312@gmail.com>
2024-07-31 19:43:11 +0900

    bgColor

--- src/hooks/useBgLightBlue.jsx
+++ src/hooks/useBgLightBlue.jsx
@@ -1,12 +1,23 @@
-import {  useEffect } from "react";
+import { useRouter } from "next/router";
+import {  useEffect, useMemo } from "react";
+
+
 
 
 export const useBgLightBlue =()=>{
+  const router =useRouter();
+
+  const bgColor = useMemo(()=>{
+    return router.pathname === "/" ? "lightblue" : "beige"
+  },[router.pathname])
+
+
+
   useEffect(() => {
-    document.body.style.backgroundColor = "lightblue"
+    document.body.style.backgroundColor =bgColor;
     return () => {
-      document.body.style.backgroundColor = "pink"
+      document.body.style.backgroundColor = ""
     }
-  }, [])
+  }, [bgColor])
 }
 
